3a7ad23084d1d1ae49a5ed0452246684ad02b02c
Claudia Hosu <claudia.hosu@gmail.com>
2017-09-29 16:45:01 +0100

    finish login form

--- libdefs.js
+++ libdefs.js
@@ -4,3 +4,4 @@ declare module 'react-redux' { declare var exports: any; }
 declare module 'redux' { declare var exports: any; }
 declare module 'react' { declare var exports: any; }
 declare module 'firebase' { declare var exports: any; }
+declare module 'redux-thunk' { declare var exports: any; }
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,12 +1,18 @@
 /* @flow */
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAILED,
+  LOGIN_USER
 } from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
-  password: ''
+  password: '',
+  user: null,
+  error: '',
+  loading: false
 };
 
 export default (state : Object = INITIAL_STATE, action: Object) => {
@@ -15,8 +21,20 @@ export default (state : Object = INITIAL_STATE, action: Object) => {
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload};
+    case LOGIN_USER:
+      return { ...state, loading: true, error: '' }
+    case LOGIN_USER_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+        error: '',
+        loading: false,
+        email: '',
+        password: ''
+      };
+    case LOGIN_USER_FAILED:
+      return { ...state, password: '', error: 'Authentication failed.', loading: false};
     default:
       return state;
-
   }
 }
